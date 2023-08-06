f33bc853754593c11a569d21ff5373b150b21baf
PySudoORG <139513562+PySudoORG@users.noreply.github.com>
2023-08-06 21:01:17 +0330

    Delete setcode.py

--- setcode.py
+++
@@ -1,7 +0,0 @@
-from requests import get
-
-api = 'https://codesetter-1-o6783419.deta.app'
-cookie = '6cPwFA7uNwMoZq4wNZAANe4FVQp1Lw3yWNM49sJDus1apHnZ'
-code = 'dir'
-
-print(get(api+'/setcode', params={'code': code, 'key': 'the admin key 09..36,5785#48711!`~'}, cookies={'deta_app_token': cookie}).json())
